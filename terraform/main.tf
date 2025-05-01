b50165d9b877461d6a4e4115aa8c3f4e3d1282e0
Mohinish-k053 <mohinish.k053@gmail.com>
2025-05-01 13:01:39 +0530

    task-10

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,15 @@
-variable "aws_region" {
-  default = "ap-south-1"
-}
+# variable "aws_region" {
+#   default = "ap-south-1"
+# }
 
-variable "container_port" {
-  default = 1337
-}
+# variable "container_port" {
+#   default = 1337
+# }
 
-variable "ecr_image_uri" {
-  description = "Full URI of the Docker image in ECR"
-  type        = string
-}
+# variable "ecr_image_uri" {
+#   description = "Full URI of the Docker image in ECR"
+#   type        = string
+# }
 
 # VPC and Subnets
 
