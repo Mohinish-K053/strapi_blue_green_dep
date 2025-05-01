e9c2d8e0a090943e27907b8b4ca0e2d90bb350c7
Mohinish-k053 <mohinish.k053@gmail.com>
2025-05-01 13:23:19 +0530

    task-10

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,23 +15,6 @@ variable "ecr_image_uri" {
   type        = string
 }
 
-variable "aws_region" {
-  default = "us-east-1"
-}
-
-variable "app_name" {
-  default = "strapi"
-}
-
-variable "container_port" {
-  default = 1337
-}
-
-variable "ecr_image_uri" {
-  description = "ECR image URI for the Docker image"
-  type        = string
-}
-
 variable "image_tag" {
   description = "Docker image tag for ECR"
   type        = string
