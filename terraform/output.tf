d7591f004a63da2f992638d1109daaf31eb49a16
Mohinish-k053 <mohinish.k053@gmail.com>
2025-05-02 13:03:42 +0530

    fix most of the issues

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,11 @@
 output "alb_dns_name" {
   value = aws_lb.strapi_alb.dns_name
 }
+
+output "ecs_service_name" {
+  value = aws_ecs_service.strapi_service.name
+}
+
+output "codedeploy_app_name" {
+  value = aws_codedeploy_app.strapi.name
+}
